4ab3f9b3be972c6a4b76be94796a0b0da75947ce
MarcosGrzeca <marcos-a-g@hotmail.com>
2016-09-25 14:20:08 -0300

    Alteração conectar na base de dados enem

--- consumidor.R
+++ consumidor.R
@@ -5,24 +5,35 @@ library(arules)
 source(file="C:\\Users\\Marcos\\Documents\\GitHub\\R-testes\\functions.R")
 
 #Configurações
-DATABASE <- "consumidor"
-str(DATABASE)
+DATABASE <- "enem"
 
 clearConsole();
-dadosBrutos <- query("SELECT NomeFantasia, Assunto, Problema, AvaliacaoReclamacao, Sexo FROM consumidor")
-str(dadosBrutos)
+#dadosBrutos <- query("SELECT MesAbertura, SegmentoMercado, NomeFantasia, AvaliacaoReclamacao FROM consumidor")
+
+dadosBrutos <- query("SELECT SEXO, IDADE, UF_ESC FROM enem")
+
+
+#Discretizar mês abertura
+#dadosBrutos$MesAbertura <- discretize(dadosBrutos$MesAbertura, method = "frequency", 12)
+#dadosBrutos$MesAbertura <- discretize(dadosBrutos$MesAbertura, categories = 12)
+
 
 #Converter caracteres para factor
 dados <- as.data.frame(unclass(dadosBrutos))
+str(dados)
 
 #Selecionar atributos
 #dados <- subset(dadosBrutos, select=c("NomeFantasia", "Assunto", "Problema", "AvaliacaoReclamacao", "Sexo"))
 clearConsole();
 
 #Execução apriori
-#rules <- apriori(titanic,parameter = list(minlen=2, supp=0.05, conf=0.8), control = list(verbose=F))
+#rules <- apriori(dados,parameter = list(minlen=2, supp=0.04, conf=0.7), control = list(verbose=F))
 #rules <- apriori(titanic,parameter = list(minlen=2, supp=0.005, conf=0.8), control = list(verbose=F))
-rules <- apriori(dados, parameter = list(minlen=2, supp=0.001, conf=0.5), appearance = list(rhs=c("AvaliacaoReclamacao=Não Resolvida"), default="lhs"), control = list(verbose=F))
+#rules <- apriori(dados, parameter = list(minlen=2, supp=0.001, conf=0.5), appearance = list(rhs=c("AvaliacaoReclamacao=Não Resolvida"), default="lhs"), control = list(verbose=F))
+#rules <- apriori(dados, parameter = list(minlen=2, supp=0.001, conf=0.5), control = list(verbose=F))
+#rules <- apriori(dados, parameter = list(minlen=2, supp=0.001, conf=0.5), appearance = list(rhs=c("AvaliacaoReclamacao="), default="lhs"), control = list(verbose=F))
+
+rules <- apriori(dados, parameter = list(minlen=2, supp=0.04, conf=0.5), appearance = list(rhs=c("Area=Telecomunicações"), default="lhs"), control = list(verbose=F))
 
 #Reordenar regras
 rules.sorted <- sort(rules, by="lift")
