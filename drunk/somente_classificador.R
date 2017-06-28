14f3fd67bf36d217aaeb089ff23232dc7e7a3fc9
MarcosGrzeca <marcos-a-g@hotmail.com>
2017-06-27 22:21:57 -0300

    ajuste texto SVM

--- drunk/somente_classificador.R
+++ drunk/somente_classificador.R
@@ -3,6 +3,6 @@ source(file_path_as_absolute("functions.R"))
 
 load("alemao_base_completa.Rda")
      
-print("NAIVE BAYES")
+print("SVM")
 source(file_path_as_absolute("classificadores.R"))
 classificar(dadosFinal)
