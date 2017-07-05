88e39de6a605acdc67c495f4f0abdf01e932d524
MarcosGrzeca <marcos-a-g@hotmail.com>
2017-07-04 23:31:34 -0300

    Ajuste scripts

--- drunk/bat _glm.sh
+++ drunk/bat _glm.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 cd /home/ec2-user/R-testes/drunk/
-Rscript /home/ec2-user/R-testes/drunk/glm.R > results/glm/result_$(date -d "today" +"%Y%m%d%H%M").txt 2> results/glm/erro_$(date -d "today" +"%Y%m%d%H%M").txt
+Rscript /home/ec2-user/R-testes/drunk/glm.R > resultados/glm/result_$(date -d "today" +"%Y%m%d%H%M").txt 2> resultados/glm/erro_$(date -d "today" +"%Y%m%d%H%M").txt
--- drunk/bat_svm.sh
+++ drunk/bat_svm.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 cd /home/ec2-user/R-testes/drunk/
-Rscript /home/ec2-user/R-testes/drunk/svm.R > results/svm/result_$(date -d "today" +"%Y%m%d%H%M").txt 2> results/svm/erro_$(date -d "today" +"%Y%m%d%H%M").txt
+Rscript /home/ec2-user/R-testes/drunk/svm.R > resultados/svm/result_$(date -d "today" +"%Y%m%d%H%M").txt 2> resultados/svm/erro_$(date -d "today" +"%Y%m%d%H%M").txt
