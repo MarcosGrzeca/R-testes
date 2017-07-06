96d8b4dc03d205ba7aeee45fad98f9e7e0170190
MarcosGrzeca <marcos-a-g@hotmail.com>
2017-07-05 21:42:13 -0300

    Ajuste no classificador default

--- drunk/classificador_default.R
+++ drunk/classificador_default.R
@@ -17,35 +17,13 @@ bh_pred <- predict(fit, dadosFinal)
 save(bh_pred, file=PATH_PRED)
 
 print("Resultados")
-
-precision <- posPredValue(bh_pred, dadosFinal$alc)
-print(paste("Precision", precision, sep=" "))
-recall <- sensitivity(bh_pred, dadosFinal$alc)
-print(paste("Recall", recall, sep=" "))
-F1 <- (2 * precision * recall) / (precision + recall)
-print(paste("F1", F1, sep=" "))
-
 a <- table(bh_pred, dadosFinal$alc)
-
-uarA <- a[1,1] / (a[1,1] + a[1,2])
-uarNA <- a[2,2] / (a[2,2] + a[2,1])
+print(a)
+uarA <- a[1,1] / (a[1,1] + a[2,1])
+uarNA <- a[2,2] / (a[2,2] + a[1,2])
 if (uarNA == "NaN"){
   uarNA = 0
 }
 uar = (uarA + uarNA) / 2
 uar
-
-
-print("Resultados MARCOS")
-precisao <- a[2,2]  / (a[2,2] + a[1,2])
-print(paste("Precision", precisao, sep=" "))
-
-revocacao <- a[2,2] / (a[2,2] + a[2,1])
-print(paste("Revocação", revocacao, sep=" "))
-
-f1 <- (2*precisao*revocacao)/(precisao+revocacao)
-print(paste("F1", f1, sep=" "))
-save.image(file="resultados/image_q2.RData")
-
 save.image(file=PATH_IMAGE)
-#stopCluster(cl)
--- drunk/svm.R
+++ drunk/svm.R
@@ -4,7 +4,6 @@ PATH_FIT <- "resultados/svm/fit.Rda"
 PATH_PRED <- "resultados/svm/pred.Rda"
 PATH_IMAGE <- "resultados/svm/image.RData"
 
-
 load("rda/alemao_base_completa.Rda")
 
 print("SVM")
@@ -12,7 +11,7 @@ print("SVM")
 library(caret)
 
 trainAlgoritmo <- function(dadosP) {
-  fit_nv <- train(x = dadosP[,2:ncol(dadosP)], 
+  fit_nv <- train(x = subset(dadosP, select = -c(alc)),
                   y = dadosP$alc, 
                   method = "svmRadial", 
                   trControl = trainControl(method = "cv", number = 10)
@@ -20,4 +19,20 @@ trainAlgoritmo <- function(dadosP) {
   return (fit_nv)
 }
 
-source(file_path_as_absolute("classificador_default.R"))
+#source(file_path_as_absolute("classificador_default.R"))
+
+#load(PATH_FIT)
+#load(PATH_PRED)
+
+teste <- function() {
+  print("Resultados")
+  a <- table(bh_pred, dadosFinal$alc)
+  a
+  uarA <- a[1,1] / (a[1,1] + a[2,1])
+  uarNA <- a[2,2] / (a[2,2] + a[1,2])
+  if (uarNA == "NaN"){
+    uarNA = 0
+  }
+  uar = (uarA + uarNA) / 2
+  uar
+}
