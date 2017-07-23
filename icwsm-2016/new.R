3a50d5f62e1d4a81564d59ac2cdf252765d69009
MarcosGrzeca <marcos-a-g@hotmail.com>
2017-07-23 11:14:15 -0300

    Versao que funciona do modelo preditivo

--- icwsm-2016/new.R
+++ icwsm-2016/new.R
@@ -2,30 +2,49 @@ load(file="denovo_99_tntando.Rda")
 
 library(tools)
 library(caret)
+
 library(doMC)
 
-registerDoMC(8)
+registerDoMC(6)
 
-split=0.75
+#dim(maFinal)
+#nzv <- nearZeroVar(maFinal)
+#nzv
+#maFinal <- maFinal[, -nzv]
+#dim(maFinal)
+
+
+set.seed(10)
+split=0.70
 trainIndex <- createDataPartition(maFinal$resposta, p=split, list=FALSE)
-data_train <- maFinal[ trainIndex,]
+data_train <- as.data.frame(unclass(maFinal[ trainIndex,]))
 data_test <- maFinal[-trainIndex,]
 
 
 print("Treinando")
 fit <- train(x = subset(data_train, select = -c(resposta)),
              y = data_train$resposta, 
-             method = "nb", 
-             trControl = trainControl(method = "cv", number = 10)
+             method = "svmLinear", 
+             trControl = trainControl(method = "cv", number = 5, savePred=T)
+             #,preProc=c("center", "scale", "nzv")
 ) 
 fit
 
-if (!require("mlbench")) {
-  install.packages("mlbench")
-}
-library(mlbench)
-importance <- varImp(fit, scale=FALSE)
-head(importance, top = 40)
-plot(importance, top = 40)
+#if (!require("mlbench")) {
+#  install.packages("mlbench")
+#}
 
-save.image(file="bora20.Rda")
+#library(mlbench)
+#importance <- varImp(fit, scale=FALSE)
+#head(importance, top = 40)
+#plot(importance, top = 40)
+
+#save.image(file="bora20.Rda")
+#load("bora20.Rda")
+
+#data_test$Class <- as.character(data_test$resposta)
+
+#aa <- predict(fit, subset(maFinal, select = -c(resposta)))
+#confusionMatrix(data = aa, maFinal$resposta)
+aa <- predict(fit, subset(data_test, select = -c(resposta)))
+confusionMatrix(data = aa, data_test$resposta, positive="1")
